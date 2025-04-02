25de2957259b6b316a08bc28ba3862ea0b1b6bed
Johnny Ly <hi@johnny.ly>
2025-04-02 10:32:22 +0200

    moved admin.js, added functions in api.js

--- src/utils/api.js
+++ src/utils/api.js
@@ -5,7 +5,6 @@ export function getBaseUrl() {
   return "http://localhost:3000/";
 }
 
-
 export async function fetchProducts(endpoint = "api/products") {
   //! DONT USE THIS IN PRODUCTION
   const url = `${getBaseUrl()}${endpoint}`;
@@ -17,9 +16,30 @@ export async function fetchProducts(endpoint = "api/products") {
   return [];    
 }
 
-
 export async function fetchCategories(endpoint = "api/categories") {
   //! DONT USE THIS IN PRODUCTION 
+  const url = `${getBaseUrl()}${endpoint}`;
+  const response = await fetch(url);
+  if(response.ok){
+    const data = await response.json();
+    return data;
+  }
+  return [];    
+}
+
+export async function fetchUsers(endpoint = "api/users") {
+  //! DONT USE THIS IN PRODUCTION
+  const url = `${getBaseUrl()}${endpoint}`;
+  const response = await fetch(url);
+  if(response.ok){
+    const data = await response.json();
+    return data;
+  }
+  return [];    
+}
+
+export async function fetchOrders(endpoint = "api/orders") {
+  //! DONT USE THIS IN PRODUCTION
   const url = `${getBaseUrl()}${endpoint}`;
   const response = await fetch(url);
   if(response.ok){
