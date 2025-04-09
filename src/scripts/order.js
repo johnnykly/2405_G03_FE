c1a66c2d1926a0e7a2aeccc135d2ec61d4c4e715
OzAlbayrak <oz@albayrak.dev>
2025-04-09 11:33:29 +0200

    Add order list and order form

--- src/scripts/order.js
+++ src/scripts/order.js
@@ -1,2 +1,35 @@
-import { fetchProducts, fetchCategories } from "../utils/api.js";
+let shoppingCart = JSON.parse(localStorage.getItem("Products"));
+const cartContainer = document.querySelector("#shoppingCartContent");
 
+cartContainer.innerHTML = "";
+const element = document.createElement("h2");
+element.innerHTML = `Varukorg`;
+cartContainer.appendChild(element);
+
+if (shoppingCart) {
+  shoppingCart.forEach((product) => {
+    const element = document.createElement("div");
+    element.className = "shopping-cart-item";
+    element.innerHTML = `
+    <div class="shopping-cart-item-info">
+        <div>${product.title}</div>
+        <div>${product.price.toFixed(2)} kr</div>
+    </div> 
+    `;
+    cartContainer.appendChild(element);
+  });
+
+  let totOrderPrice = 0;
+  shoppingCart.forEach((product) => {
+    totOrderPrice += product.price;
+  });
+  const element = document.createElement("div");
+  element.className = "shopping-cart-item";
+  element.innerHTML = `
+    <div class="shopping-cart-item-info-tot-price">
+        <div>Totalt order pris:</div>
+        <div>${totOrderPrice.toFixed(2)} kr</div>
+    </div> 
+    `;
+  cartContainer.appendChild(element);
+}
